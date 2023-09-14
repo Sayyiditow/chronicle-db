32b3c544d636d3abf24df0f9e78bfb3a240a16af
hashim <hashim@sayyiditow.dev>
2023-09-14 12:52:46 +0800

    initial commit

--- src/test/java/chronicle/db/LeadDaoTest.java
+++ src/test/java/chronicle/db/LeadDaoTest.java
@@ -12,9 +12,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -24,7 +22,6 @@ import com.jsoniter.JsonIterator;
 import com.jsoniter.output.JsonStream;
 import com.jsoniter.spi.TypeLiteral;
 
-import chronicle.db.config.Globals;
 import chronicle.db.entity.Search;
 import chronicle.db.entity.Search.SearchType;
 
@@ -32,11 +29,12 @@ import chronicle.db.entity.Search.SearchType;
 public class LeadDaoTest {
     private final String objectClassName = "chronicle.db.service.Lead";
     private final String daoClassName = "chronicle.db.service.LeadDao";
+    private static final String DATA_PATH = "src/main/resources/.data/chronicle/";
 
     @Test
     public void testInsert() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
             NoSuchFieldException, SecurityException, IOException, InstantiationException, InvocationTargetException {
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         final var c = CHRONICLE_DB.getObjectConstructor(objectClassName);
         final String sourcePath = "/home/hashim/Downloads/leads/dat.json.aa";
         final List<Map<String, Object>> source = JsonIterator.deserialize(Files.readAllBytes(Paths.get(sourcePath)),
@@ -70,7 +68,7 @@ public class LeadDaoTest {
             IllegalAccessException, NoSuchFieldException, SecurityException, IntrospectionException,
             InstantiationException, InvocationTargetException {
         final long time = System.currentTimeMillis();
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         System.out.println(dao.size());
         final var searchValues = dao.search(new Search("fullName", Search.SearchType.LIKE, "pheakdey ton"));
         System.out.println(JsonStream.serialize(searchValues));
@@ -81,7 +79,7 @@ public class LeadDaoTest {
     @Test
     public void index() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
             NoSuchFieldException, SecurityException, IOException, InstantiationException, InvocationTargetException {
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         dao.initIndex("fullName");
     }
 
@@ -89,7 +87,7 @@ public class LeadDaoTest {
     public void indexedSearch() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
             NoSuchFieldException, SecurityException, IOException, InstantiationException, InvocationTargetException {
         final long time = System.currentTimeMillis();
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         final var map = dao.indexedSearch(new Search("fullName", SearchType.EQUAL, "pheakdey ton"));
         final long end = System.currentTimeMillis();
         System.out.println(end - time);
@@ -97,50 +95,9 @@ public class LeadDaoTest {
     }
 
     @Test
-    public void testJoin() {
-        final ConcurrentMap<String, Customer> map = new ConcurrentHashMap<>();
-        map.put("test", new Customer("hashim", 1));
-        map.put("test3", new Customer("hashim2", 2));
-        final ConcurrentMap<String, Invoice> map2 = new ConcurrentHashMap<>();
-        map2.put("1", new Invoice("test", "abcd12"));
-        map2.put("2", new Invoice("test", "abcd23"));
-        map2.put("3", new Invoice("test", "abcd45"));
-        map2.put("4", new Invoice("test3", "abcd67"));
-        map2.put("5", new Invoice("test3", "abcd78"));
-        map2.put("6", new Invoice("test4", "abcd89"));
-        final List<Object> list = new ArrayList<>();
-
-        // inner join
-        map2.forEach((key, value) -> {
-            final ConcurrentMap<String, Object> valueMap = new ConcurrentHashMap<>();
-            final Customer customer = map.get(value.customerNo());
-            if (Objects.nonNull(customer)) {
-                valueMap.put("Name", customer.name());
-                valueMap.put("Type", customer.type());
-                valueMap.put("Invoice Id", key);
-                list.add(valueMap);
-            }
-        });
-        System.out.println(list);
-        list.clear();
-
-        // left join
-        map2.forEach((k, v) -> {
-            final ConcurrentMap<String, Object> valueMap = new ConcurrentHashMap<>();
-            Customer cust = map.get(v.customerNo());
-            cust = cust == null ? new Customer() : cust;
-            valueMap.put("Name", cust.name());
-            valueMap.put("Type", cust.type());
-            valueMap.put("Invoice Id", k);
-            list.add(valueMap);
-        });
-        System.out.println(list);
-    }
-
-    @Test
     public void testMultiThreadDelete() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
             SecurityException, ClassNotFoundException, IOException, InstantiationException, InvocationTargetException {
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         System.out.println(dao.size());
         dao.delete(new HashSet<>(Arrays.asList("1-99000", "2-99001")));
         System.out.println(dao.size());
@@ -150,7 +107,7 @@ public class LeadDaoTest {
     @Test
     public void testGetAll() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
             NoSuchFieldException, SecurityException, IOException, InstantiationException, InvocationTargetException {
-        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, Globals.DATA_PATH);
+        final var dao = CHRONICLE_DB.getMultiChronicleDao(daoClassName, DATA_PATH);
         System.out.println(dao.get(new HashSet<>(Arrays.asList("0-10", "0-11"))));
     }
 }
