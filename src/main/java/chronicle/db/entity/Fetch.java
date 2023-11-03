4adcf9612ab4630e96a58f8edbed88bfee20a4d1
hashim <hashim@sayyiditow.dev>
2023-11-03 11:50:03 +0800

    data path should be known and not set

--- src/main/java/chronicle/db/entity/Fetch.java
+++ src/main/java/chronicle/db/entity/Fetch.java
@@ -4,13 +4,12 @@ import java.util.HashSet;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record Fetch(String objectName, String dataPath, Object key, HashSet<?> keys, Search search, int limit,
+public record Fetch(String objectName, Object key, HashSet<?> keys, Search search, int limit,
         String[] subsetFields) {
     @JsonCreator
-    public Fetch(final String objectName, final String dataPath, final Object key, final HashSet<?> keys,
+    public Fetch(final String objectName, final Object key, final HashSet<?> keys,
             final Search search, final int limit, final String[] subsetFields) {
         this.objectName = objectName;
-        this.dataPath = dataPath;
         this.key = key;
         this.keys = keys;
         this.limit = limit;
