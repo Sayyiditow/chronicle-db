3fe07dd2bc2dddfb608cf519f2f355df5e1fc629
hashim <hashim@sayyiditow.dev>
2023-11-03 19:46:21 +0800

    rename to headers

--- src/main/java/chronicle/db/entity/CsvObject.java
+++ src/main/java/chronicle/db/entity/CsvObject.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record CsvObject(String[] header, List<Object[]> rows) {
+public record CsvObject(String[] headers, List<Object[]> rows) {
     @JsonCreator
-    public CsvObject(final String[] header, final List<Object[]> rows) {
-        this.header = header;
+    public CsvObject(final String[] headers, final List<Object[]> rows) {
+        this.headers = headers;
         this.rows = rows;
     }
 }
