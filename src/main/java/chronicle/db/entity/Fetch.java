da687ab2c70907570ade7b965b0ac94872a09279
hashim <hashim@sayyiditow.dev>
2023-11-08 14:13:12 +0800

    add ability to do multi search

--- src/main/java/chronicle/db/entity/Fetch.java
+++ src/main/java/chronicle/db/entity/Fetch.java
@@ -4,11 +4,11 @@ import java.util.HashSet;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record Fetch(String objectName, Object key, HashSet<?> keys, Search search, int limit,
+public record Fetch(String objectName, Object key, HashSet<?> keys, Search[] search, int limit,
         String[] subsetFields) {
     @JsonCreator
     public Fetch(final String objectName, final Object key, final HashSet<?> keys,
-            final Search search, final int limit, final String[] subsetFields) {
+            final Search[] search, final int limit, final String[] subsetFields) {
         this.objectName = objectName;
         this.key = key;
         this.keys = keys;
