1354d14ab448fac34f00ca3f9fbbfdd483f33b34
hashim <hashim@sayyiditow.dev>
2023-12-17 22:06:31 +0800

    add path prefix for objects being reused

--- src/main/java/chronicle/db/entity/Fetch.java
+++ src/main/java/chronicle/db/entity/Fetch.java
@@ -4,11 +4,12 @@ import java.util.HashSet;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record Fetch(String objectName, Object key, HashSet<?> keys, Search[] search, int limit,
+public record Fetch(String pathPrefix, String objectName, Object key, HashSet<?> keys, Search[] search, int limit,
         String[] subsetFields) {
     @JsonCreator
-    public Fetch(final String objectName, final Object key, final HashSet<?> keys,
+    public Fetch(final String pathPrefix, final String objectName, final Object key, final HashSet<?> keys,
             final Search[] search, final int limit, final String[] subsetFields) {
+        this.pathPrefix = pathPrefix;
         this.objectName = objectName;
         this.key = key;
         this.keys = keys;
