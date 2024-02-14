fad892f1d0790c1726fcc148cb74b06781a4e0c8
hashim <hashim@kollect.biz>
2024-02-14 12:16:40 +0800

    remove file lock

--- src/main/java/chronicle/db/service/MapDb.java
+++ src/main/java/chronicle/db/service/MapDb.java
@@ -21,6 +21,7 @@ public final class MapDb {
                 .fileMmapEnableIfSupported() // Only enable mmap on supported platforms
                 .fileMmapPreclearDisable()   // Make mmap file faster
                 .cleanerHackEnable()
+                .fileLockDisable()
                 .make();
         Logger.info("Opening index file at: {}", filePath);
         return (HTreeMap<K, V>) db.hashMap("map").createOrOpen();
