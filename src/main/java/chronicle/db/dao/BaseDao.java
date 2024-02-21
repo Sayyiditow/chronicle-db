36e54cc9d3f3bcb745990dc540be514f7d2b0c88
hashim <hashim@kollect.biz>
2024-02-21 11:06:45 +0800

    class name

--- src/main/java/chronicle/db/dao/BaseDao.java
+++ src/main/java/chronicle/db/dao/BaseDao.java
@@ -340,7 +340,7 @@ interface BaseDao<K, V> {
 
     @SuppressWarnings("unchecked")
     private void castSet(final Object searchTerm, Set<Object> set) {
-        System.out.println(searchTerm.getClass());
+        System.out.println(searchTerm.getClass().getName());
         if (searchTerm instanceof ArrayList)
             set = new HashSet<>((ArrayList<Object>) searchTerm);
         else
