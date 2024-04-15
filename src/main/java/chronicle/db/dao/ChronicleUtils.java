5c4e403c70fb2ca598f2781f99b6bedb96e23d4d
hashim <hashim@kollect.biz>
2024-04-15 15:42:24 +0800

    try parse to long if the field is long

--- src/main/java/chronicle/db/dao/ChronicleUtils.java
+++ src/main/java/chronicle/db/dao/ChronicleUtils.java
@@ -92,7 +92,8 @@ public final class ChronicleUtils {
         if (Objects.nonNull(field)) {
             final Object searchTerm = field.getType().isEnum() && (search.searchTerm() instanceof String)
                     ? toEnum(field.getType(), search.searchTerm())
-                    : search.searchTerm();
+                    : field.getType() == Long.class ? Long.parseLong(search.searchTerm().toString())
+                            : search.searchTerm();
             final Object currentValue = field.get(value);
 
             if (Objects.nonNull(currentValue)) {
