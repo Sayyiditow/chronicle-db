5d0bd915ceb0637e28b2ddf18758f88130989898
hashim <hashim@kollect.biz>
2024-11-18 10:40:52 +0800

    implement serializable for over the socket transfer

--- src/main/java/chronicle/db/entity/CsvObject.java
+++ src/main/java/chronicle/db/entity/CsvObject.java
@@ -1,10 +1,11 @@
 package chronicle.db.entity;
 
+import java.io.Serializable;
 import java.util.List;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record CsvObject(String[] headers, List<Object[]> rows) {
+public record CsvObject(String[] headers, List<Object[]> rows) implements Serializable {
     @JsonCreator
     public CsvObject(final String[] headers, final List<Object[]> rows) {
         this.headers = headers;
