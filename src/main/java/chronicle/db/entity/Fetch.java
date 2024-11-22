eaa3e59bbc8df366579cae73a52ba70275a564b4
hashim <hashim@kollect.biz>
2024-11-22 16:26:47 +0800

    changed

--- src/main/java/chronicle/db/entity/Fetch.java
+++ src/main/java/chronicle/db/entity/Fetch.java
@@ -1,14 +1,15 @@
 package chronicle.db.entity;
 
 import java.util.HashSet;
+import java.util.List;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record Fetch(String pathPrefix, String objectName, Object key, HashSet<?> keys, Search[] search, int limit,
+public record Fetch(String pathPrefix, String objectName, Object key, HashSet<?> keys, List<Search> search, int limit,
         String[] subsetFields) {
     @JsonCreator
     public Fetch(final String pathPrefix, final String objectName, final Object key, final HashSet<?> keys,
-            final Search[] search, final int limit, final String[] subsetFields) {
+            final List<Search> search, final int limit, final String[] subsetFields) {
         this.pathPrefix = pathPrefix;
         this.objectName = objectName;
         this.key = key;
