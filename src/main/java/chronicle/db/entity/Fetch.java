64ebcbca051bd63a1eb6c3fe2d2dd36bc6e24451
hashim <hashim@kollect.biz>
2025-02-17 14:38:18 +0800

    WIP

--- src/main/java/chronicle/db/entity/Fetch.java
+++ src/main/java/chronicle/db/entity/Fetch.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 import com.jsoniter.annotation.JsonCreator;
 
-public record Fetch(String pathPrefix, String objectName, Object key, Set<?> keys, List<Search> search, int limit,
+public record Fetch(String pathPrefix, String objectEnum, Object key, Set<?> keys, List<Search> search, int limit,
         String[] subsetFields) {
     @JsonCreator
-    public Fetch(final String pathPrefix, final String objectName, final Object key, final Set<?> keys,
+    public Fetch(final String pathPrefix, final String objectEnum, final Object key, final Set<?> keys,
             final List<Search> search, final int limit, final String[] subsetFields) {
         this.pathPrefix = pathPrefix;
-        this.objectName = objectName;
+        this.objectEnum = objectEnum;
         this.key = key;
         this.keys = keys;
         this.limit = limit;
