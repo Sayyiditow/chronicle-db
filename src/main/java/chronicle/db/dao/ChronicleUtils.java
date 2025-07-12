7f6d48af2ccfc12f0a05ef032fa7710ad593dc53
hashim <hashim@kollect.biz>
2025-07-12 15:41:58 +0800

    log less

--- src/main/java/chronicle/db/dao/ChronicleUtils.java
+++ src/main/java/chronicle/db/dao/ChronicleUtils.java
@@ -261,7 +261,7 @@ public final class ChronicleUtils {
         }
 
         if (db.isEmpty()) {
-            Logger.info("DB is empty. Index files created but skipping data indexing at [{}].", indexDirPath);
+            Logger.info("DB is empty. Index files created at [{}].", indexDirPath);
             openIndexes.forEach((indexPath, indexDb) -> MAP_DB.closeIndex(indexPath));
             return;
         }
